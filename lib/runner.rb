8f9be790d4bfd7f1348563d8b3fd70bdaa76c316
Jonathan Garvin <jon@5valleys.com>
2010-11-09 22:30:11 -0700

    successfully makeing initial hourly backup on first run

--- lib/runner.rb
+++ lib/runner.rb
@@ -7,17 +7,23 @@ module Syncophant
       
       def run(path_to_config = nil, job_name = nil)
         load_config(path_to_config,job_name)
-        initialize_folders
+        initialize_root_frequency_folders
+        return if previous_hourly_target == current_hourly_target  #already ran within the last hour
+        run_backups
       end
       
       def load_config(path_to_config = nil, job_name = nil)
         @settings = YAML.load_file(path_to_config || 'config/config.yml')[job_name ||  'default']
       end
       
-      def initialize_folders
+      def initialize_root_frequency_folders
         FREQUENCIES.each do |frequency|
-          Dir.mkdir(send("root_#{frequency}_path")) unless File.exists?(send("root_#{frequency}_path")) && File.directory?(send("root_#{frequency}_path"))
+          Dir.mkdir(send("root_#{frequency}_target")) unless File.exists?(send("root_#{frequency}_target")) && File.directory?(send("root_#{frequency}_target"))
         end
+      end
+      
+      def source
+        @settings['source']
       end
       
       def target
@@ -25,12 +31,25 @@ module Syncophant
       end
       
       FREQUENCIES.each do |frequency|
-        define_method("root_#{frequency}_path") do
+        define_method("root_#{frequency}_target") do
           "#{target}/#{frequency}"
         end
       end
       
+      def current_hourly_target
+        t = Time.now
+        "#{root_hourly_target}/#{sprintf("%04d-%02d-%02d.%02d", t.year, t.month, t.day, t.hour)}"
+      end
       
+      def previous_hourly_target
+         @previous_hourly_target = Dir["#{root_hourly_target}/*"].sort{|a,b| File.ctime(b) <=> File.ctime(a) }.first
+      end
+     
+      def run_backups
+        if previous_hourly_target.nil?
+          system 'rsync', '-a', source, current_hourly_target          
+        end
+      end
     end
   end
 end
--- test/unit/tc_runner.rb
+++ test/unit/tc_runner.rb
@@ -6,13 +6,12 @@ class TestSyncophantRunner < Test::Unit::TestCase
  
   def setup
     @settings = Syncophant::Runner.load_config('test/config/config.yml')
-    FileUtils.rm_rf(@settings['target'])
+    FileUtils.rm_rf(@settings['target']) if File.exists?(@settings['target'])
     Dir.mkdir(@settings['target'])
   end
   
   def teardown
     FileUtils.rm_rf(@settings['target'])
-    Dir.mkdir(@settings['target'])
   end
   
   def test_load_config
@@ -21,7 +20,16 @@ class TestSyncophantRunner < Test::Unit::TestCase
  
   def test_initialize_directories
     assert_equal(false,File.exists?("#{@settings['target']}/hourly"))
-    Syncophant::Runner.initialize_folders
+    Syncophant::Runner.initialize_root_frequency_folders
     assert_equal(true,File.exists?("#{@settings['target']}/hourly"))
+  end
+  
+  def test_run
+    t = Time.now
+    assert_equal(true,File.exists?("#{@settings['source']}/README"))
+    assert_equal(false,File.exists?("#{@settings['target']}/hourly/#{sprintf("%04d-%02d-%02d.%02d", t.year, t.month, t.day, t.hour)}/source/README"))
+    Syncophant::Runner.run('test/config/config.yml')
+    assert_equal(true,File.exists?("#{@settings['target']}/hourly/#{sprintf("%04d-%02d-%02d.%02d", t.year, t.month, t.day, t.hour)}/source/README"))
+    
   end
  end
